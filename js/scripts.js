8324bf718c292ed8be784b183c45de5fe8d1274f
Alex Caste <alex.castaneda@gmail.com>
2015-05-30 00:13:44 -0700

    remove input checker function because regex only searches words

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,13 @@
 var replacer = function(sentence, wordFind, wordReplace) {
-  var findCheck = inputChecker(wordFind)
-  var replaceCheck = inputChecker(wordReplace)
   var wordFind = nonCharacter(wordFind)
   var wordReplace = nonCharacter(wordReplace)
-  if (findCheck === false &&  replaceCheck === false ) {
     var searchWord = "\\b(" + wordFind + ")\\b";
     var finder = new RegExp(searchWord, "gi");
     var newSentence = sentence.replace(finder, wordReplace);
-  } else {
-    return "We can only search or replace whole words.";
-  };
 
   return newSentence;
 };
 
-var inputChecker = function(wordFind) {
-  var wordArray = wordFind.split("");
-  if (wordArray.length === 1) {
-    return true;
-  } else {
-    return false;
-  };
-};
 
 var nonCharacter = function(word) {
   var notLetterNumber = /[^a-z0-9]|\s+|\r?\n|\r/gi
